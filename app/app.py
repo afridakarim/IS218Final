681dfd332ef8ef4688ed9884a3ec5ac5c424600a
afridakarim <ak2577@njit.edu>
2020-05-06 22:05:12 -0400

    Add: app.py update

--- app/app.py
+++ app/app.py
@@ -41,21 +41,21 @@ def form_edit_get(addr_id):
     return render_template('edit.html', title='Edit Form', city=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['POST'])
-def form_update_post(city_id):
+@app.route('/edit/<int:addr_id>', methods=['POST'])
+def form_update_post(addr_id):
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('LatD'), request.form.get('LatM'), request.form.get('LatS'),
-                 request.form.get('NS'), request.form.get('LonD'),
-                 request.form.get('LonM'), request.form.get('LonS'),request.form.get('EW'),request.form.get('City'),request.form.get('State'), city_id)
-    sql_update_query = """UPDATE cities_table.cities_csv t SET t.LatD = %s, t.LatM = %s, t.LatS = %s, t.NS = 
-    %s, t.LonD = %s, t.LonM = %s, t.LonS = %s, t.EW = %s, t.City = %s, t.State = %s WHERE t.id = %s """
+    inputData = (request.form.get('first'), request.form.get('last'), request.form.get('Address'),
+                 request.form.get('City'), request.form.get('State'),
+                 request.form.get('zip'), addr_id)
+    sql_update_query = """UPDATE tblAddress t SET t.first = %s, t.last = %s, t.Address = %s, t.City = 
+    %s, t.State = %s, t.zip = %s WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-@app.route('/cities/new', methods=['GET'])
+@app.route('/address/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New City Form')
+    return render_template('new.html', title='New Address Form')
 
 
 @app.route('/cities/new', methods=['POST'])
@@ -63,11 +63,13 @@ def form_insert_post():
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('LatD'), request.form.get('LatM'), request.form.get('LatS'),
                  request.form.get('NS'), request.form.get('LonD'),
-                 request.form.get('LonM'), request.form.get('LonS'), request.form.get('EW'), request.form.get('City'), request.form.get('State'))
+                 request.form.get('LonM'), request.form.get('LonS'), request.form.get('EW'), request.form.get('City'),
+                 request.form.get('State'))
     sql_insert_query = """INSERT INTO cities_table.cities_csv (LatD,LatM,LatS,NS,LonD,LonM,LonS,EW,City,State) VALUES (%s,%s,%s,%s,%s, %s,%s,%s,%s,%s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
+
 
 @app.route('/delete/<int:city_id>', methods=['POST'])
 def form_delete_post(city_id):
@@ -76,9 +78,6 @@ def form_delete_post(city_id):
     cursor.execute(sql_delete_query, city_id)
     mysql.get_db().commit()
     return redirect("/", code=302)
-
-
-
 
 
 if __name__ == '__main__':
